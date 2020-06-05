5c6f36bea0be744855a31078f70c750c2140b3a8
sandeep-chahal <sandeepchahal573@gmail.com>
2020-06-05 13:16:42 +0530

    fix set view engine mistake render pug template fix port add home.pug

--- app.js
+++ app.js
@@ -8,22 +8,25 @@ const easyPath = require("./utils/easyPath")(__dirname);
 const app = express();
 
 //setting view engine
-app.set("view engine", easyPath("pug"));
+app.set("view engine", "pug");
 app.set("views", easyPath("./views"));
 //serving static files
 app.use(express.static(easyPath("./public")));
 
 //routes
 app.get("/", (req, res, next) => {
-	res.send("Hellooo!");
+	console.log("gsdgsfg");
+	res.status(200).render("home");
 });
 
 // connect to db and start the server
-console.log(process.env.MONGO_URI);
 mongoose
-	.connect(process.env.MONGO_URI)
+	.connect(process.env.MONGO_URI, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
 	.then(() => {
-		app.listen(process.env.PORT);
+		app.listen(process.env.PORT || 3000);
 	})
 	.catch((err) => {
 		console.log(err);
