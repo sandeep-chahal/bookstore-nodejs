e2393cbd8dab63047f6c86243807875c20da0030
sandeep-chahal <sandeepchahal573@gmail.com>
2020-06-06 09:13:20 +0530

    update User model

--- models/user.js
+++ models/user.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		trim: true,
+		unique: [true, "Email is already in use!"],
+		required: [true, "Enter your email!"],
+		maxLength: [50, "Email is too long!"],
+	},
 	name: {
 		type: String,
 		trim: true,
 		required: [true, "please enter your name!"],
 		minlength: [3, "name must be greater then 3 characters"],
 		maxLength: [30, "name must be less then 30 characters"],
+	},
+	password: {
+		type: String,
+		required: [true, "Please enter password!"],
+		maxlength: [50, "password too long!"],
 	},
 	selling: [{ type: mongoose.Types.ObjectId, ref: "Book" }],
 });
